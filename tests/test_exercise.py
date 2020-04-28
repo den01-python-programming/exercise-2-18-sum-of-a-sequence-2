ad6482292d45661a748757aa79ab435cfd75d2b7
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 11:15:16 +0100

    Updates exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,21 @@
 import pytest
-from src.exercise import main
+import src.exercise
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    input_values = ["9","12"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert output == ["First number?","Last number?","The sum is 42"]
